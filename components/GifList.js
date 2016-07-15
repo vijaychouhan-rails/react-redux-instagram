049b71463da794d1f4d5ba4f33c688c30a0504d5
rails <rails@rails.com>
2016-07-15 16:49:51 +0530

    application with react

--- components/GifList.js
+++ components/GifList.js
@@ -3,7 +3,7 @@ import GifItem from './GifItem';
 
 const GifList = (props) => {
   const gifItems = props.gifs.map((image) => {
-    return <GifItem key={image.id} gif={image} />
+    return <GifItem key={image.id} gif={image} onGifSelect={props.onGifSelect} />
   });
 
   return (
